8ebd0eca996d04c1fc87d459e8a4dbefa2864178
achaisa <achaisa@unsa.edu.pe>
2026-05-30 16:27:13 -0500

    pruebas test 4

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -3,7 +3,7 @@ from src.calculadora import dividir
 
 # === 1. PRUEBAS DE CAMINO FELIZ (HAPPY PATH) ===
 def test_dividir_enteros_positivos():
-    assert dividir(10, 2) == 5.0
+    assert dividir(10, 2) == 1.0
 
 def test_dividir_resultado_decimal():
     assert dividir(5, 2) == 2.5
